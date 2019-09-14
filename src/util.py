b19ada83b173bc434938d3937d56a0cb27cf0b66
Austin Doolittle <austindoolittle@gmail.com>
2019-09-14 13:31:06 -0400

    Set log status

--- src/util.py
+++ src/util.py
@@ -8,4 +8,5 @@ def get_logger(name):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
     return logger
